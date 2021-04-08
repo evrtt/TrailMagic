96b17f213ce6109b15f8bc39402a612190f9609a
heavyevy <everettsmith95@gmail.com>
2021-04-07 21:07:23 -0700

    Install fontawesome and create footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,9 @@ import { Link, Route, Switch } from 'react-router-dom'
 import LoginFormContainer from './session/login_form_container';
 import SignUpFormContainer from './session/signup_form_container';
 import { AuthRoute, ProtectedRoute } from '../utils/route_util';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+
 
 export default () => (
   <section className="main">
@@ -13,16 +16,30 @@ export default () => (
         </nav>
         <div className="logo-container">
           <Link to="/" className="logo-link">
-            <img className="logo" src={window.logoURL}/>
-            <span>TrailMagic</span>
+            <span>Trail</span>
+            <img className="logo-header" src={window.logoURL}/>
+            <span>Magic</span>
           </Link>
         </div>
         <WelcomeContainer />
     </header>
-    <Switch>
-      <AuthRoute exact path='/login' component={LoginFormContainer} />
-      <AuthRoute exact path='/signup' component={SignUpFormContainer} />
-      {/* <Route exact path='/' component={SplashContainer} /> */}
-    </Switch>
+    <section className="main-section">
+      <Switch>
+        <AuthRoute exact path='/login' component={LoginFormContainer} />
+        <AuthRoute exact path='/signup' component={SignUpFormContainer} />
+        {/* <Route exact path='/' component={SplashContainer} /> */}
+      </Switch>
+    </section>
+    <footer className="footer">
+      <Link to="/" className="logo-link">
+        <img className="logo" src={window.logoURL} />
+        <span>TrailMagic</span>
+      </Link>
+      <div className="icons">
+        <FontAwesomeIcon icon={faLinkedin} />
+        <FontAwesomeIcon icon={faGithub} />
+      </div>
+
+    </footer>
   </section>
 );
