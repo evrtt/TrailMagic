fd3c4cecd5b6c0d5cbbcc9a57e7599153d74dc9e
heavyevy <everettsmith95@gmail.com>
2021-04-10 13:51:16 -0700

    Create trail model

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,2 +1,13 @@
 class Trail < ApplicationRecord
+  
+  validates 
+    :title, 
+    :route_coords,
+    :difficulty,
+    :location,
+    :length,
+    :description,
+    :estimated_time,
+    presence: true
+
 end
