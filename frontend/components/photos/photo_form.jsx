8142645fc998bb0ff53372b990293db0a885a088
heavyevy <everettsmith95@gmail.com>
2021-06-03 13:39:08 -0700

    Hide stock input type file button and replace on photos_form

--- frontend/components/photos/photo_form.jsx
+++ frontend/components/photos/photo_form.jsx
@@ -15,6 +15,9 @@ class PhotosForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFile = this.handleFile.bind(this);
     this.clearAddedPhotos = this.clearAddedPhotos.bind(this);
+    this.openFile = this.openFile.bind(this);
+
+    this.fileInputRef = React.createRef();
   }
 
   handleSubmit(e) {
@@ -44,47 +47,78 @@ class PhotosForm extends React.Component {
     })
   }
 
+  openFile() {
+    this.fileInputRef.current.click()
+  }
+
   render() {
     let preview;
-    if(this.state.photoURL) {
-      preview = <div className="image-preview">
-        <img src={this.state.photoURL}/> 
-      </div>
-    } else {
-      preview = null
-    }
     let submitButton;
-    if(this.state.photoFile) {
-      submitButton = <div className='photos-buttons-container'>
-        <button
-          className='photos-button-cancel'
-          onClick={this.clearAddedPhotos}
-        >
-          Cancel
+    let browseFiles;
+    if(this.state.photoURL && this.state.photoFile) {
+      preview = (
+        <div className="image-preview">
+          <img src={this.state.photoURL}/> 
+        </div>
+      );
+      submitButton = (
+        <div className='photos-buttons-container'>
+          <button
+            className='photos-button-cancel'
+            onClick={this.clearAddedPhotos}
+          >
+            Cancel
+          </button>
+          <button
+            className='photos-button-ready'
+            onClick={this.handleSubmit}
+          >
+            Upload
+          </button>
+        </div>
+      );
+      browseFiles = (
+        <button onClick={this.openFile} className="change-file-container">
+          <span>
+            Choose a different file
+          </span>
+          <p>
+            PNG, JPG, or JPEG
+          </p>
         </button>
-        <button
-          className='photos-button-ready'
-          onClick={this.handleSubmit}
-        >
-          Upload
-        </button>
-      </div>
+      );
     } else {
-      submitButton = <div className='photos-buttons-container'>
-        <button className='photos-button-not-ready'>
-          Upload 
+      preview = null;
+      submitButton = (
+        <div className='photos-buttons-container'>
+          <button className='photos-button-not-ready'>
+            Upload 
+          </button>
+        </div>
+      );
+      browseFiles = (
+        <button onClick={this.openFile} className="browse-files-container">
+          <FontAwesomeIcon icon={faCamera} className="inner-camera" />
+          <span>
+            Browse files
+          </span>
+          <p>
+            PNG, JPG, or JPEG
+          </p>
         </button>
-      </div>
-      
+      );
     }
     return (
       <form className="upload-photos-form">
         <label>Upload photo</label>
         {preview}
+        {browseFiles}
         <input
           type="file"
           onChange={this.handleFile}
           style={{display: "hidden"}}
+          ref={this.fileInputRef}
+          style={{display: "none"}}
         />
         {submitButton}
       </form>
